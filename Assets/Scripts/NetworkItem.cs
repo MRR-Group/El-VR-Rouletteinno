2c297150e6fbacd31b89f65dd3c93e8dbd677e63
Dominik Purgał <amonsir29@gmail.com>
2025-03-19 15:44:23 +0100

    Enable item CanUse

--- Assets/Scripts/NetworkItem.cs
+++ Assets/Scripts/NetworkItem.cs
@@ -59,8 +59,6 @@ public abstract class NetworkItem : NetworkBehaviour
     
     protected bool CanUse()
     {
-        return true;
-        
         return GameManager.Instance.GameState == GameState.IN_PROGRESS && GameManager.Instance.turn.IsClientTurn();
     }
 
