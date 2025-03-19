51cd33a762f59734908f7dc90f83095e995071b7
Dominik Purgał <amonsir29@gmail.com>
2025-03-19 18:57:22 +0100

    wip

--- Assets/Scripts/HideOtherPlayerUi.cs
+++ Assets/Scripts/HideOtherPlayerUi.cs
@@ -1,17 +1,28 @@
 using System;
 using UnityEngine;
 
+<<<<<<< Updated upstream
 public class HideOtherPlayerUi : MonoBehaviour
 {
     [SerializeField]
     private GameObject m_player;
+=======
+[RequireComponent(typeof(GameChair))]
+public class HideOtherPlayerUi : MonoBehaviour
+{
+    [SerializeField]
+    private GameChair m_player;
+>>>>>>> Stashed changes
     
     [SerializeField]
     private GameObject m_menu;
     
     private void Start()
     {
+<<<<<<< Updated upstream
         
+=======
+>>>>>>> Stashed changes
         GameManager.Instance.GameStateChanged += GameManager_OnGameStateChanged;
     }
 
