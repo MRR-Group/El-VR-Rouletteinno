7cea3ab1ed69f0f3fe102e7211821aca788ae61c
KacperWalenga <kacper.walenga@studenci.collegiumwitelona.pl>
2025-03-28 03:03:19 +0100

    Add energy dring sound effect

--- Assets/Scripts/Inventory/Items/EnergyDrink.cs
+++ Assets/Scripts/Inventory/Items/EnergyDrink.cs
@@ -3,6 +3,9 @@ using Unity.Netcode;
 
 public class EnergyDrink : NetworkItem
 {
+    [SerializeField]
+    private AudioSource m_drinkAudio;
+    
     protected override bool CanUse(ulong player)
     {
         return base.CanUse(player) && !GameManager.Instance.Round.Gun.IsMagazineEmpty();
@@ -12,6 +15,15 @@ public class EnergyDrink : NetworkItem
     {
         GameManager.Instance.Round.Gun.SkipCurrentBulletRpc();
 
+        PlayDrinkAudioRpc();
+
         return true;
+    }
+
+    [Rpc(SendTo.Everyone)]
+    private void PlayDrinkAudioRpc()
+    {
+        m_drinkAudio.time = 0;
+        m_drinkAudio.Play();
     }
 }
